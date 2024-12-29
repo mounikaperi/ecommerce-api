e3bfb5ae60c1d696ffd9826fbfd62e8724d200d5
mounikaperi <saimounikaperi@gmail.com>
2024-12-29 21:20:35 +0530

    Login and Logout User Functionality

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 const cloudinary = require('cloudinary');
 const User = require('../models/userModel');
+const asyncErrorHandler = require('../middlewares/asyncErrorHandler');
+const ErrorHandler = require('../utils/ErrorHandler');
 
 exports.registerUser = asyncErrorHandler(async (req, res, next) => {
   const avatarUploadedToCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
@@ -20,3 +22,32 @@ exports.registerUser = asyncErrorHandler(async (req, res, next) => {
   });
   sendToken(newlyCreatedUser, 201, res);
 });
+
+exports.loginUser = asyncErrorHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter Email and Password", 400));
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+  sendToken(user, 201, res);
+});
+
+exports.logoutUser = asyncErrorHandler(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out"
+  })
+});
+
+
